0fad33435008fac9a3bdddfccf1057990b2da627
CassioEnrique <22041107@itdurango.edu.mx>
2026-08-28 21:29:06 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return render_template("index.html")
+
+@app.route("/saludar", methods=["POST"])
+def saludar():
+    nombre = request.form["nombre"]
+    pasatiempos = request.form.getlist("pasatiempos")
+    me_gusta = request.form.get("me_gusta", "")
+    
+    return render_template(
+        "saludar.html",
+        nombre=nombre,
+        pasatiempos=pasatiempos,
+        me_gusta=me_gusta
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
